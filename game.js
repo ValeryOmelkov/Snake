36eb1bdd49f806e35054434bc571f348124c522c
ValeryOmelkov <omelkov_valera@mail.ru>
2021-07-19 23:41:04 +0500

    Обработано столкновение с препятствиями

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 class Game {
     constructor() {
+        this._isStarted = false;
         this._speed = 100;
         this._sizeX = 17;
         this._sizeY = 17;
@@ -8,13 +9,18 @@ class Game {
         this._height = this._sizeY * this._sizeCell;
         this._canvas = document.getElementById('canvas');
         this._ctx = this._canvas.getContext('2d');
-        this._snake = new Snake([5, 5], this._sizeCell, this._ctx);
-        this._drawField();
-        this._snake.drawSnake();
+        document.addEventListener('keypress', this._onKeyPress.bind(this));
+        this._snake = new Snake([8, 8], this._sizeCell, this._ctx);
         this._createFruit();
-        setInterval(() => {
-            this._snake.moveSnake();
+        this._drawField();
+        this._drawFruit();
+        this._snake.drawSnake();
+    }
+    _process() {
+        this._processInterval = setInterval(() => {
+            this._moveSnake();
             this._drawField();
+            this._drawFruit();
             this._snake.drawSnake();
         }, this._speed);
     }
@@ -26,38 +32,102 @@ class Game {
             }
         }
     }
+    // TODO: Сделать генерацию не под змейкой
     _createFruit() {
-        const x = Math.round(Math.random() * (this._sizeX));
-        const y = Math.round(Math.random() * (this._sizeY));
+        const x = Math.floor(Math.random() * (this._sizeX));
+        const y = Math.floor(Math.random() * (this._sizeY));
         this._fruitCoord = [x, y];
+    }
+    _drawFruit() {
         this._ctx.fillStyle = '#AA0000';
-        this._ctx.fillRect(x * this._sizeCell, y * this._sizeCell, this._sizeCell, this._sizeCell);
+        this._ctx.fillRect(this._fruitCoord[0] * this._sizeCell, this._fruitCoord[1] * this._sizeCell, this._sizeCell, this._sizeCell);
+    }
+    _checkFruit(snake) {
+        if (snake[0][0] === this._fruitCoord[0] && snake[0][1] === this._fruitCoord[1]) {
+            return true;
+        }
+        return false;
+    }
+    _moveSnake() {
+        const direction = this._snake.getDirection();
+        const oldSnake = this._snake.getSnake();
+        let currentSnake = [];
+        switch (direction) {
+            case 'up':
+                currentSnake[0] = [oldSnake[0][0], oldSnake[0][1] - 1];
+                break;
+            case 'down':
+                currentSnake[0] = [oldSnake[0][0], oldSnake[0][1] + 1];
+                break;
+            case 'right':
+                currentSnake[0] = [oldSnake[0][0] + 1, oldSnake[0][1]];
+                break;
+            case 'left':
+                currentSnake[0] = [oldSnake[0][0] - 1, oldSnake[0][1]];
+                break;
+        }
+        for (let i = 1; i < oldSnake.length; i++) {
+            currentSnake[i] = oldSnake[i - 1];
+        }
+        if (this._checkFruit(oldSnake)) {
+            this._createFruit();
+            currentSnake.push(oldSnake[oldSnake.length - 1]);
+        }
+        if (this._checkIsAlive(currentSnake)) {
+            this._snake.setSnake(currentSnake);
+        }
+        else {
+            this._endGame();
+        }
+    }
+    _checkIsAlive(snake) {
+        const head = snake[0]; // Координаты головы змеи
+        // Проверка на столкновение со стеной
+        if ((head[0] < 0) || (head[0] >= this._sizeX) || (head[1] < 0) || (head[1] >= this._sizeY)) {
+            return false;
+        }
+        // Проверка на столкновение с туловищем
+        for (const item of snake.slice(1)) {
+            if (item[0] === head[0] && item[1] === head[1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    _onKeyPress(event) {
+        switch (event.key) {
+            case 'w':
+                this._snake.up();
+                break;
+            case 'a':
+                this._snake.left();
+                break;
+            case 's':
+                this._snake.down();
+                break;
+            case 'd':
+                this._snake.right();
+                break;
+            default: return;
+        }
+        if (!this._isStarted) {
+            this._process();
+            this._isStarted = true;
+        }
+    }
+    _endGame() {
+        clearInterval(this._processInterval);
+        alert('Проигрышь!');
     }
 }
 class Snake {
     constructor(coord, sizeCell, ctx) {
         this._snake = [];
+        this._isAlive = true;
         this._sizeCell = sizeCell;
         this._ctx = ctx;
-        document.addEventListener('keypress', this._onKeyPress.bind(this));
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 3; i++) {
             this._snake.push([coord[0] + i, coord[1]]);
-        }
-    }
-    _onKeyPress(event) {
-        switch (event.key) {
-            case 'w':
-                this._direction = this._direction != 'down' ? 'up' : 'down';
-                break;
-            case 'a':
-                this._direction = this._direction != 'right' ? 'left' : 'right';
-                break;
-            case 's':
-                this._direction = this._direction != 'up' ? 'down' : 'up';
-                break;
-            case 'd':
-                this._direction = this._direction != 'left' ? 'right' : 'left';
-                break;
         }
     }
     drawSnake() {
@@ -68,26 +138,26 @@ class Snake {
             this._ctx.fillRect(this._snake[i][0] * this._sizeCell, this._snake[i][1] * this._sizeCell, this._sizeCell, this._sizeCell);
         }
     }
-    moveSnake() {
-        if (this._direction) {
-            for (let i = this._snake.length - 1; i > 0; i--) {
-                this._snake[i] = this._snake[i - 1];
-            }
-        }
-        switch (this._direction) {
-            case 'up':
-                this._snake[0] = [this._snake[0][0], this._snake[0][1] - 1];
-                break;
-            case 'down':
-                this._snake[0] = [this._snake[0][0], this._snake[0][1] + 1];
-                break;
-            case 'right':
-                this._snake[0] = [this._snake[0][0] + 1, this._snake[0][1]];
-                break;
-            case 'left':
-                this._snake[0] = [this._snake[0][0] - 1, this._snake[0][1]];
-                break;
-        }
+    up() {
+        this._direction = this._direction != 'down' ? 'up' : 'down';
+    }
+    left() {
+        this._direction = this._direction != 'right' ? 'left' : 'right';
+    }
+    down() {
+        this._direction = this._direction != 'up' ? 'down' : 'up';
+    }
+    right() {
+        this._direction = this._direction != 'left' ? 'right' : 'left';
+    }
+    getSnake() {
+        return this._snake;
+    }
+    setSnake(value) {
+        this._snake = value;
+    }
+    getDirection() {
+        return this._direction;
     }
 }
 const game = new Game();
--- game.ts
+++ game.ts
@@ -1,25 +1,37 @@
 class Game{
     protected _canvas: any;
     protected _ctx: any;
+
+    protected _isStarted: boolean = false;
+
     protected _speed: number = 100;
     protected _sizeX: number = 17;
     protected _sizeY: number = 17;
     protected _sizeCell: number = 30;
     protected _width: number = this._sizeX * this._sizeCell;
     protected _height: number = this._sizeY * this._sizeCell;
-    protected _fruitCoord: Array<number>;
+
+    protected _processInterval: number;
+    protected _fruitCoord: [number, number];
     protected _snake: Snake;
+    protected _points: number;
 
     constructor(){
         this._canvas = document.getElementById('canvas'); 
         this._ctx = this._canvas.getContext('2d');
-        this._snake = new Snake([5,5], this._sizeCell, this._ctx);
-        this._drawField();
-        this._snake.drawSnake();
+        document.addEventListener('keypress', this._onKeyPress.bind(this));
+        this._snake = new Snake([8,8], this._sizeCell, this._ctx);
         this._createFruit();
-        setInterval (() => {
-            this._snake.moveSnake();
+        this._drawField();
+        this._drawFruit();
+        this._snake.drawSnake();
+    }
+
+    protected _process(): void{
+        this._processInterval = setInterval (() => {
+            this._moveSnake();
             this._drawField();
+            this._drawFruit();
             this._snake.drawSnake();
         }, this._speed);
     }
@@ -32,37 +44,102 @@ class Game{
             }
         }
     }
-
+    // TODO: Сделать генерацию не под змейкой
     protected _createFruit(): void{
-        const x = Math.round(Math.random() * (this._sizeX));
-        const y = Math.round(Math.random() * (this._sizeY));
+        const x = Math.floor(Math.random() * (this._sizeX));
+        const y = Math.floor(Math.random() * (this._sizeY));
         this._fruitCoord = [x, y];
-        this._ctx.fillStyle = '#AA0000';
-        this._ctx.fillRect(x*this._sizeCell, y*this._sizeCell, this._sizeCell, this._sizeCell);
     }
-}
 
-class Snake{
-    protected _snake: Array<Array<number>> = [];
-    protected _sizeCell: number;
-    protected _ctx: any;
-    protected _direction: string; // up | down | right | left
+    protected _drawFruit(): void{
+        this._ctx.fillStyle = '#AA0000';
+        this._ctx.fillRect(this._fruitCoord[0]*this._sizeCell, this._fruitCoord[1]*this._sizeCell, this._sizeCell, this._sizeCell);
+    }
 
-    constructor(coord: Array<number>, sizeCell: number, ctx: any){
-        this._sizeCell = sizeCell;
-        this._ctx = ctx;
-        document.addEventListener('keypress', this._onKeyPress.bind(this));
-        for(let i: number = 0; i < 4; i++){
-           this._snake.push([coord[0]+i, coord[1]]);
+    protected _checkFruit(snake: Array<[number, number]>): boolean{
+        if (snake[0][0] === this._fruitCoord[0] && snake[0][1] === this._fruitCoord[1]){
+            return true;
         }
+        return false;
+    }
+
+    protected _moveSnake(): void{
+        const direction: string = this._snake.getDirection();
+        const oldSnake: Array<[number, number]> = this._snake.getSnake();
+        let currentSnake: Array<[number, number]> = [];
+        
+        switch(direction){
+            case 'up': currentSnake[0] = [oldSnake[0][0], oldSnake[0][1] - 1]; break;
+            case 'down': currentSnake[0] = [oldSnake[0][0], oldSnake[0][1] + 1]; break;
+            case 'right': currentSnake[0] = [oldSnake[0][0] + 1, oldSnake[0][1]]; break;
+            case 'left': currentSnake[0] = [oldSnake[0][0] - 1, oldSnake[0][1]]; break;
+        }
+    
+        for(let i: number = 1; i < oldSnake.length; i++){
+            currentSnake[i] = oldSnake[i-1];
+        }
+
+        if(this._checkFruit(oldSnake)){
+            this._createFruit();
+            currentSnake.push(oldSnake[oldSnake.length-1]);
+        }
+
+        if(this._checkIsAlive(currentSnake)){
+            this._snake.setSnake(currentSnake);
+        } else {
+            this._endGame();
+        }
+    }
+
+    protected _checkIsAlive(snake: Array<[number, number]>): boolean{
+        const head: [number, number] = snake[0]; // Координаты головы змеи
+        // Проверка на столкновение со стеной
+        if ((head[0] < 0) || (head[0] >= this._sizeX) || (head[1] < 0) || (head[1] >= this._sizeY)){
+            return false;
+        }
+        // Проверка на столкновение с туловищем
+        for(const item of snake.slice(1)){
+            if(item[0] === head[0] && item[1] === head[1]){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     protected _onKeyPress(event: any): void{
         switch(event.key){
-            case 'w': this._direction = this._direction != 'down' ? 'up' : 'down'; break;
-            case 'a': this._direction = this._direction != 'right' ? 'left' : 'right'; break;
-            case 's': this._direction = this._direction != 'up' ? 'down' : 'up'; break;
-            case 'd': this._direction = this._direction != 'left' ? 'right' : 'left'; break;
+            case 'w': this._snake.up(); break;
+            case 'a': this._snake.left(); break;
+            case 's': this._snake.down(); break;
+            case 'd': this._snake.right(); break;
+            default: return;
+        }
+
+        if(!this._isStarted) {
+            this._process();
+            this._isStarted = true;
+        }
+    }
+
+    protected _endGame(): void{
+        clearInterval(this._processInterval);
+        alert('Проигрышь!');
+    }
+}
+
+class Snake{
+    protected _ctx: any;
+    protected _snake: Array<[number, number]> = [];
+    protected _sizeCell: number;
+    protected _direction: string; // up | down | right | left
+    protected _isAlive: boolean = true;
+
+    constructor(coord: [number, number], sizeCell: number, ctx: any){
+        this._sizeCell = sizeCell;
+        this._ctx = ctx;
+        for(let i: number = 0; i < 3; i++){
+           this._snake.push([coord[0]+i, coord[1]]);
         }
     }
 
@@ -75,18 +152,32 @@ class Snake{
         }
     }
 
-    public moveSnake(): void{
-        if (this._direction){
-            for(let i: number = this._snake.length - 1; i > 0; i--){
-                this._snake[i] = this._snake[i-1];
-            }
-        }
-        switch(this._direction){
-            case 'up': this._snake[0] = [this._snake[0][0], this._snake[0][1] - 1]; break;
-            case 'down': this._snake[0] = [this._snake[0][0], this._snake[0][1] + 1]; break;
-            case 'right': this._snake[0] = [this._snake[0][0] + 1, this._snake[0][1]]; break;
-            case 'left': this._snake[0] = [this._snake[0][0] - 1, this._snake[0][1]]; break;
-        }
+    public up(): void{
+        this._direction = this._direction != 'down' ? 'up' : 'down';
+    }
+
+    public left(): void{
+        this._direction = this._direction != 'right' ? 'left' : 'right';
+    }
+
+    public down(): void{
+        this._direction = this._direction != 'up' ? 'down' : 'up';
+    }
+
+    public right(): void{
+        this._direction = this._direction != 'left' ? 'right' : 'left';
+    }
+
+    public getSnake(): Array<[number, number]>{
+        return this._snake;
+    }
+
+    public setSnake(value: Array<[number, number]>): void{
+        this._snake = value;
+    }
+
+    public getDirection(): string{
+        return this._direction;
     }
 }
 
